04a5c33edc42d628a72e027fc033f6d3d8a12a07
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 17:36:36 +0100

    Added navbar

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/about">About</NuxtLink>
+          <NuxtLink to="/products">Products</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <!-- Slot in page content -->
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 1rem 0;
+    padding: 0;
+  }
+
+  li > *:not(:first-child) {
+    margin-left: 1rem;
+  }
+
+  a {
+    text-underline-offset: 0.1em;
+    transition: text-decoration-color 300ms, text-underline-offset 300ms;
+  }
+
+  a:hover {
+    color: black;
+    text-underline-offset: 0.2em
+  }
+
+  .router-link-active {
+    color: green;
+    text-decoration-color: black;
+  }
+</style>
