0244f90b08062050c4c6326c1137b804c4eccd02
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 17:11:53 +0100

    Mock products & prodcuts/id pages

---
+++ pages/products/[productId].vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  const route = useRoute()
+  const productId = route.params.productId
+</script>
+
+
+
+<template>
+  <h2>Product details for {{ productId }}</h2>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quam voluptas cupiditate? Sit facilis sapiente non velit illum culpa!</p>
+</template>
+
+
+
+<style scoped>
+  h2 {
+    margin: 25px 0;
+    font-size: 30px;
+  }
+</style>
