e95d90b6aa375c6eb58b4517e1be537da22ddb66
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 17:02:11 +0100

    Setup mock home & about page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+
+
+<template>
+  <h2>Home Page</h2>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quam voluptas cupiditate? Sit facilis sapiente non velit illum culpa!</p>
+</template>
+
+
+
+<style scoped>
+  h2 {
+    margin: 25px 0;
+    font-size: 30px;
+  }
+</style>
