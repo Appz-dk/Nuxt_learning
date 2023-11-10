2891dfde76637ef3dd971dc375c33a505305356a
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 21:44:31 +0100

    Added custom tailwind btn class

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <template>
-  <header class="container flex justify-between mx-auto p-4 shadow-sm">
+  <header class="container flex justify-between mx-auto p-4 shadow-sm bg-white">
     <NuxtLink class="font-bold custom-green" to="/"
     ><h1>Nuxt App</h1></NuxtLink>
     <nav>
-      <ul class="flex gap-4">
+      <ul class="flex items-center gap-4">
         <NavLink to="/">
           Home
         </NavLink>
         <NavLink to="/about">
           About
         </NavLink>
-        <NavLink to="/products">
-          Products
-        </NavLink>
+        <li>
+          <NuxtLink to="/products" class="btn">Products</NuxtLink>
+        </li>
       </ul> 
     </nav>
   </header>
--- layouts/products.vue
+++ layouts/products.vue
@@ -1,19 +1,19 @@
 <!-- This layout is identical to default.vue but wanted to try out definePageMeta -->
 <template>
-   <header class="container flex justify-between mx-auto p-4 shadow-sm">
+   <header class="container flex justify-between mx-auto p-4 shadow-sm bg-white">
     <NuxtLink class="font-bold custom-green" to="/"
     ><h1>Nuxt App</h1></NuxtLink>
     <nav>
-      <ul class="flex gap-4">
+      <ul class="flex items-center gap-4">
         <NavLink to="/">
           Home
         </NavLink>
         <NavLink to="/about">
           About
         </NavLink>
-        <NavLink to="/products">
-          Products
-        </NavLink>
+        <li>
+          <NuxtLink to="/products" class="btn">Products</NuxtLink>
+        </li>
       </ul> 
     </nav>
   </header>
