0201df361774f1484dd165da7d84c8f660ade513
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 23:05:20 +0100

    Refactor product cards into component

---
+++ components/ProductCard.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import type { TProduct } from '~/types/types';
+  const { product } = defineProps({
+    product: {
+      type: Object as PropType<TProduct>,
+      required: true
+    }
+  })
+
+  const router = useRouter()
+  const viewProductDetails = (productId: number) => {
+    router.push(`/products/${productId}`)
+  }
+</script>
+
+
+
+<template>
+  <div class="card gap-6 w-80 flex flex-col items-center">
+    <img class="object-contain max-h-36 rounded-md" :src="product.image"/>
+    <h3 class="font-semibold line-clamp-2">{{ product.title }}</h3>
+    <div class="mt-auto">
+      <button class="btn" @click="viewProductDetails(product.id)">View Details</button>
+    </div>
+  </div>
+</template>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -6,11 +6,6 @@
   })
 
   const {data: products} = await useFetch<TProduct[]>('https://fakestoreapi.com/products')
-
-  const router = useRouter()
-  const viewProductDetails = (productId: number) => {
-    router.push(`/products/${productId}`)
-  }
 </script>
 
 
@@ -18,13 +13,7 @@
 <template>
   <h2>Products page</h2>
   <div class="grid grid-cols-4 gap-y-6 place-items-center">
-    <div v-for="product in products" class="p-4 h-80 w-80 bg-white rounded-md gap-4 flex flex-col items-center">
-      <img class="object-contain max-h-36 rounded-md" :src="product.image"/>
-      <h3 class="font-semibold line-clamp-2">{{ product.title }}</h3>
-      <div class="mt-auto">
-        <button class="btn" @click="viewProductDetails(product.id)">View Details</button>
-      </div>
-    </div>
+    <ProductCard v-for="product in products" :product="product"/>
   </div>
 </template>
 
