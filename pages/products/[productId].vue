59ce952d4fad1f08cc3447d07b430fa54d250dc9
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 22:45:27 +0100

    Working on [productId] page

--- pages/products/[productId].vue
+++ pages/products/[productId].vue
@@ -1,18 +1,31 @@
 <script lang="ts" setup>
+  import type { TProduct } from '~/types/types';
+
   const route = useRoute()
   const productId = route.params.productId
 
   definePageMeta({
     layout: 'products'
   })
+
+  const url = `https://fakestoreapi.com/products/${productId}`
+  const {data: product, pending} = useFetch<TProduct>(url)
 </script>
 
 
 
-<template>
-  <h2>Product details for {{ productId }}</h2>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quam voluptas cupiditate? Sit facilis sapiente non velit illum culpa!</p>
-</template>
+<template >
+  <div v-if="pending">
+    <h2>Loading product...</h2>
+  </div>
+  <div v-else-if="product">
+    <h2>Product details for {{ product.id }}</h2>
+    <p>{{ product.description }}</p>
+  </div>
+  <div v-else>
+    <h2>Sorry we could not find your product...</h2>
+  </div>
+  </template>
 
 
 
