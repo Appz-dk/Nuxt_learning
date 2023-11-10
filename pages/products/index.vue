46c6856426d417e9455cdfa00df163879bb1d06b
Appz-dk <patrick.fyhn@gmail.com>
2023-11-10 22:18:25 +0100

    Setup fakestoreapi & display products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,13 +2,38 @@
   definePageMeta({
     layout: 'products'
   })
+
+  type TProduct = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    rating: { count: number, rate: number },
+    title: string
+  }
+
+  const {data: products} = await useFetch<TProduct[]>('https://fakestoreapi.com/products')
+
+  const router = useRouter()
+  const viewProductDetails = (productId: number) => {
+    router.push(`/products/${productId}`)
+  }
 </script>
 
 
 
 <template>
   <h2>Products page</h2>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quam voluptas cupiditate? Sit facilis sapiente non velit illum culpa!</p>
+  <div class="grid grid-cols-4 gap-y-6 place-items-center">
+    <div v-for="product in products" class="p-4 h-80 w-80 bg-white rounded-md gap-4 flex flex-col items-center">
+      <img class="object-contain max-h-36 rounded-md" :src="product.image"/>
+      <h3 class="font-semibold line-clamp-2">{{ product.title }}</h3>
+      <div class="mt-auto">
+        <button class="btn" @click="viewProductDetails(product.id)">View Details</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 
