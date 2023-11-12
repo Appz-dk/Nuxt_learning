8a6074abeb5d37c0c0caf7a11b9b0f43004a3f98
Appz-dk <patrick.fyhn@gmail.com>
2023-11-12 16:57:57 +0100

    Added text to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,15 @@
 
 <template>
   <h2>Home Page</h2>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aliquid quam voluptas cupiditate? Sit facilis sapiente non velit illum culpa!</p>
+  <p class="text-lg leading-relaxed text-gray-700">
+    Welcome to my learning journey with Nuxt.js! This project showcases my exploration of Nuxt and its features. 
+  </p>
+  <p class="text-lg leading-relaxed text-gray-700">
+    I setup some basic routing with the Home and About pages. These pages, while quite empty, serve as a practical exercise in navigating the framework.
+  </p>
+  <p class="text-lg leading-relaxed text-gray-700">
+    On the Products page, I use Nuxt's dynamic routing. I fetch a collection of products from the fakestoreapi, using the useFetch method and manage page interactions using Nuxt.
+  </p>
 </template>
 
 
