ab123ae675003e2251d936f18fda0bd7121b6659
Appz-dk <patrick.fyhn@gmail.com>
2023-11-12 17:15:50 +0100

    Added 'tooltips' to clamped product titles

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-  import type { TProduct } from '~/types/types';
+  import type { VNodeRef } from 'vue';
+import type { TProduct } from '~/types/types';
   const { product } = defineProps({
     product: {
       type: Object as PropType<TProduct>,
@@ -11,6 +12,15 @@
   const viewProductDetails = (productId: number) => {
     router.push(`/products/${productId}`)
   }
+
+  // Controll if product title needs a 'tooltip'
+  const pTitle = ref<HTMLElement>()
+  const productTitleTooltip = () => {
+    // Comparing the scrollHeight and the clientHeight of the "clamped" element
+    if (pTitle.value && pTitle.value.scrollHeight > pTitle.value.clientHeight) return pTitle.value.innerText
+    return ""
+  }
+
 </script>
 
 
@@ -18,7 +28,7 @@
 <template>
   <div class="card gap-6 w-72 flex flex-col items-center">
     <img class="object-contain max-h-36 rounded-md" :src="product.image"/>
-    <h3 class="font-semibold line-clamp-2">{{ product.title }}</h3>
+    <h3 ref="pTitle" class="font-semibold line-clamp-2" :title="productTitleTooltip()">{{ product.title }}</h3>
     <div class="mt-auto">
       <button class="btn" @click="viewProductDetails(product.id)">View Details</button>
     </div>
